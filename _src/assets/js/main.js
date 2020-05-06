682e4a10899bad194854125033530ce7a9ba7a9f
cristinapontiijas <cristinapontijas@gmail.com>
2020-05-06 17:56:54 +0200

    set html Code for list in javascript

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-console.log('holi mundi');
+// global variables
+
+const addBtn = document.querySelector('.js_btn-add');
+
+// arrays
+
+let taskLists = [];
+
+// functions
+
+function handleEvent() {
+  setHtmlListCode();
+}
+
+function setHtmlListCode() {
+  let htmlCode = '';
+
+  htmlCode += ` <section class="main__section">`;
+  htmlCode += ` <div class="card__button-container">
+      <button class="card__button">JS</button>
+      <button class="card__button">Css</button>
+      <button class="card__button">Html</button>
+    </div>`;
+  htmlCode += `<h2 class="main__title">Tarea</h2>`;
+  htmlCode += `<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>`;
+  htmlCode += `<button class="main__button">+ Añadir otra tarjeta</button>`;
+  htmlCode += `</section>`;
+
+  console.log('me han clickado');
+
+  return htmlCode;
+}
+
+addBtn.addEventListener('click', handleEvent);
